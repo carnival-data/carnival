e7c7b9ef15b713a1e737148e0c2cba4157888f42
David Birtwell <augustearth@gmail.com>
2019-09-19 12:13:23 -0400

    docs

--- docs/tutorial/basics-util.groovy
+++ docs/tutorial/basics-util.groovy
@@ -1,14 +1,12 @@
 @Grab(group='edu.upenn.pmbb', module='carnival-util', version='0.2.6')
 
 import carnival.util.MappedDataTable
-import carnival.util.KeyType
 
 def mdt = new MappedDataTable(
     name:"myMappedDataTable",
     idFieldName:'ID'
 )
-
 mdt.dataAdd(ID:'1A', NAME:'alex')
+
 def currentDir = new File(System.getProperty("user.dir"))
 mdt.writeFiles(currentDir)
-

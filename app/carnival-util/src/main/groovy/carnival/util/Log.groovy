5abec85a03e60db02d7c8c0d2c88e77bef8ffdb4
David Birtwell <augustearth@gmail.com>
2019-11-13 18:27:47 -0500

    logging...

--- app/carnival-util/src/main/groovy/carnival/util/Log.groovy
+++ app/carnival-util/src/main/groovy/carnival/util/Log.groovy
@@ -37,6 +37,11 @@ class Log {
      *
      */
     static void progress(Logger log, String logMethod, String msg, Integer total, Integer current) {
+        if (total == 0) {
+            log."$logMethod"("$msg $current of $total")
+            return
+        }
+
         log."$logMethod"("$msg $current of $total (${ Math.round(Math.floor((current*100)/total)) }%)")
     }
 

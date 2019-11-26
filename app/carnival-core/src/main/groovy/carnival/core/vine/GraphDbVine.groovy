1148dfe19f7ed9bd2d9f220abc8e655019e0c3d1
greenguy33 <hayden.freedman@gmail.com>
2019-11-25 21:53:47 -0500

    added sparql query functions

--- app/carnival-core/src/main/groovy/carnival/core/vine/GraphDbVine.groovy
+++ app/carnival-core/src/main/groovy/carnival/core/vine/GraphDbVine.groovy
@@ -16,7 +16,9 @@ import org.eclipse.rdf4j.model.impl.SimpleValueFactory
 
 import carnival.core.config.RdfConfig
 
-
+import org.eclipse.rdf4j.query.BooleanQuery
+import org.eclipse.rdf4j.query.TupleQuery
+import org.eclipse.rdf4j.query.TupleQueryResult
 
 /**
  * Convenience class that offers useful utilities for interacting with GraphDb RDF triplestores <http://graphdb.ontotext.com/>.
@@ -37,7 +39,6 @@ abstract class GraphDbVine extends Vine {
     RepositoryManager repositoryManager
     Repository repository
     RepositoryConnection connection
-
 
     ///////////////////////////////////////////////////////////////////////////
     // CONSTRUCTORS
@@ -120,4 +121,13 @@ abstract class GraphDbVine extends Vine {
     	connection.remove(subject, predicate, object)
     }
 
+    public Boolean launchSparqlBooleanQuery(String query) {
+        BooleanQuery boolResult = connection.prepareBooleanQuery(query)
+        return boolResult.evaluate()
+    }
+
+    public TupleQueryResult launchSparqlSelectQuery(String query) {
+        TupleQuery tupleQuery = connection.prepareTupleQuery(query)
+        return tupleQuery.evaluate()
+    }
 }
